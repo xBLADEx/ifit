dde4fbd03eca5c4ebd96848be6bab5b19cfe0597
Rich Edmunds <rich@richedmunds.com>
2020-05-23 10:14:33 -0600

    Adjust course, add animation.

--- src/components/course.js
+++ src/components/course.js
@@ -5,43 +5,60 @@ import iconPlaylist from "../assets/images/icon-playlist.png"
 
 const Course = props => {
   return (
-    <li
-      className={`course ${
-        props.details.shadow === false ? "course--no-shadow" : null
-      }`}
-    >
-      <div className="course__image-container">
-        <img src={props.details.image} alt="" className="course__image" />
-      </div>
-      <div className="course__details-container">
-        <div className="course__details">
-          <p className="course__name">{props.details.name}</p>
-          <ul className="course__stats">
-            {props.details.time ? (
-              <li className="course__stats-item">
-                <img src={iconTime} alt="" className="course__stats-icon" />
-                {props.details.time}
-              </li>
-            ) : null}
-            {props.details.miles ? (
-              <li className="course__stats-item">
-                <img src={iconDistance} alt="" className="course__stats-icon" />
-                {props.details.miles} M
-              </li>
-            ) : null}
-          </ul>
-          {props.details.view_more ? (
-            <a href="#" className="course__view-more">
-              View Details
-            </a>
+    <li className="course">
+      <a
+        href="#"
+        className={`course__link ${
+          props.details.shadow === false ? "course__link--no-shadow" : ""
+        }`}
+      >
+        <div className="course__image-container">
+          <img src={props.details.image} alt="" className="course__image" />
+          {props.details.workouts ? (
+            <div className="course__workout">
+              <p className="course__workout-text">
+                <span className="course__workout-number">
+                  {props.details.workouts}
+                </span>
+                Workouts
+              </p>
+
+              <img src={iconPlaylist} alt="" className="course__workout-icon" />
+            </div>
           ) : null}
         </div>
-        <img
-          src={props.details.instructor_icon}
-          alt=""
-          className="course__instructor"
-        />
-      </div>
+        <div className="course__details-container">
+          <div className="course__details">
+            <p className="course__name">{props.details.name}</p>
+            <ul className="course__stats">
+              {props.details.time ? (
+                <li className="course__stats-item">
+                  <img src={iconTime} alt="" className="course__stats-icon" />
+                  {props.details.time}
+                </li>
+              ) : null}
+              {props.details.miles ? (
+                <li className="course__stats-item">
+                  <img
+                    src={iconDistance}
+                    alt=""
+                    className="course__stats-icon"
+                  />
+                  {props.details.miles} M
+                </li>
+              ) : null}
+            </ul>
+            {props.details.view_more ? (
+              <p className="course__view-more">View Details</p>
+            ) : null}
+          </div>
+          <img
+            src={props.details.instructor_icon}
+            alt=""
+            className="course__instructor"
+          />
+        </div>
+      </a>
     </li>
   )
 }
